77cef47489e3debd96c5709a6f162961f51e907c
gudalaprathyusha <149353310+gudalaprathyusha@users.noreply.github.com>
2023-11-09 12:00:31 +0530

    Add files via upload

--- Bike.java
+++ Bike.java
@@ -1,8 +1,12 @@
-package com.tap;
+package com.coupling;
 
-public interface Bike {
-      void bikeCompany(); 
-    	 
-     }
+public class Bike implements Examination  {
 
+	@Override
+	public void gift() {
+		System.out.println("you won bike....");
+		
+	}
+	
 
+}
